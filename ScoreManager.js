60247ffa0713adbbb4616e39c3e8f61867792f71
kentar <kentar@gmail.com>
2024-03-19 22:34:30 +0100

    Добавил прогрессбар

--- ScoreManager.js
+++ ScoreManager.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     var totalPrice = 0;
     var data;
-    var currentIndex = 0; 
+    var currentIndex = 0;
 
     fetchData();
 
@@ -11,7 +11,7 @@ $(document).ready(function () {
             method: 'GET',
             dataType: 'json',
             success: function (responseData) {
-                data = responseData; 
+                data = responseData;
                 handleData(data);
             },
             error: function (error) {
@@ -21,33 +21,40 @@ $(document).ready(function () {
     }
 
     function handleData(data) {
-        
         displayWindow(0);
     }
 
-    
     function displayWindow(index) {
         let price = parseInt(data[index].price);
         totalPrice += price;
 
-        
         let windowContent = '<div class="window">' + data[index].text + '</div>';
         $('#windowsContainer').empty().append(windowContent);
 
-        
         $('.window').removeClass('active');
         $('.window').eq(index).addClass('active');
 
-        
         currentIndex = index;
-
-        
         $('#priceDisplay').text('Total Price: ' + totalPrice);
+
+        // Обновляем шкалу прогресса
+        updateProgressBar();
     }
 
-    
     $('#nextBtn').on('click', function () {
-        
-        displayWindow(currentIndex + 1);
+        // Увеличиваем индекс, чтобы перейти к следующей фразе
+        currentIndex++;
+        // Если достигнут конец массива, переходим к началу
+        if (currentIndex >= data.length) {
+            currentIndex = 0;
+        }
+        // Показываем окно с новой фразой
+        displayWindow(currentIndex);
     });
+
+    // Функция для обновления шкалы прогресса
+    function updateProgressBar() {
+        var filledSteps = Math.floor((totalPrice / 15) * 100 / (100 / 15));
+        $('#progress').css('height', (filledSteps * (100 / 15)) + '%');
+    }
 });
