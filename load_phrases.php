b60a664043f76727e973e26c04d28daac8bc29a9
kentar <kentar@gmail.com>
2024-03-21 13:58:00 +0100

    Обновил базу

--- load_phrases.php
+++ load_phrases.php
@@ -4,28 +4,27 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 header('Content-Type: application/json');
 
-
 $servername = "localhost";
 $username = "kentar"; 
 $password = "password"; 
 $dbname = "chechen";
 
-
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-
-$sql = "SELECT * FROM first_lesson";
+// Select data from the first_lesson table
+$sql = "SELECT fl.* FROM first_lesson fl INNER JOIN russian_words rw ON fl.id = rw.id";
 $result = $conn->query($sql);
 
 // Fetch data and return as JSON
 $data = array();
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
+        // Добавляем данные в массив только если id из обеих таблиц совпадает
         $data[] = $row;
     }
 }
